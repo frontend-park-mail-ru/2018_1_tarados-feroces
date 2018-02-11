ad7e9c5c39350a3d062ca7f378554882e1ed3f1e
Danchetto <danchetto@yandex.ru>
2018-02-11 14:47:49 +0300

    +history api

--- src/app/pages/history.js
+++ src/app/pages/history.js
@@ -1,6 +1,31 @@
 "use strict";
 
-const prevURL = window.history.back().url;
+const changeButton = document.getElementById("change");
 
-console.log(prevURL);
+window.addEventListener("popstate", (event) => {
+    console.log(event.state.page);
+});
 
+changeButton.addEventListener("click", () => {
+   const currentState = history.state;
+   if (currentState === null || currentState.page === "base") {
+      goNext();
+   }
+   else if (currentState.page === "test"){
+      goPrevious();
+   }
+});
+
+const goNext = () => {
+    const newState = {page: "test"};
+    history.pushState(newState, "Test", "/test");
+    document.getElementById("data").innerText = "Goodbye";
+    document.getElementById("change").innerText = "Prev";
+};
+
+const goPrevious = () => {
+    const newState = {page: "base"};
+    history.pushState(newState, "History", "/history.html");
+    document.getElementById("data").innerText = "Hello";
+    document.getElementById("change").innerText = "Next";
+};
