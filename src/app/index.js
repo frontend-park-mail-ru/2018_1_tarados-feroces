6398d4d60948fbcc689d557058663032350ecb1e
kabachok169 <kabachok890098@yandex.ru>
2018-02-13 22:24:42 +0300

    replaced creating of the router

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,4 @@
 'use strict';
 
-const router = new Router();
-
-router.addUrl('/login/', 'login-template', document.querySelector('.root'));
-router.addUrl('/auth/', 'authorized-template', document.querySelector('.root'));
-
 router.go(document.location.pathname);
 
