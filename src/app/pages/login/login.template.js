0cb02a8cdc3b50962f71d14d0668f8bf1511d595
kabachok169 <kabachok890098@yandex.ru>
2018-02-15 09:11:55 +0300

    farmate template

--- src/app/pages/login/login.template.js
+++ src/app/pages/login/login.template.js
@@ -1,15 +1,15 @@
 const loginTemplate = '<div class="login-block"> ' +
-    '<form> ' +
-    '<div class="login-block__input-block"> ' +
-    '<label class="error error-disable">Wrong login</label> ' +
-    '<label>Login:</label> ' +
-    '<input class="login__input"> ' +
-    '</div> ' +
-    '<div class="login-block__input-block"> ' +
-    '<label class="error error-disable">Wrong password</label> ' +
-    '<label>Password:</label> ' +
-    '<input type="password" class="login__input"> ' +
-    '</div> ' +
-    '<div id="1" class="login-block__login-button"><p>Sign in!</p></div> ' +
-    '</form> ' +
+        '<form> ' +
+            '<div class="login-block__input-block"> ' +
+                '<label class="error error-disable">Wrong login</label> ' +
+                '<label>Login:</label> ' +
+                '<input class="login__input"> ' +
+            '</div> ' +
+            '<div class="login-block__input-block"> ' +
+                '<label class="error error-disable">Wrong password</label> ' +
+                '<label>Password:</label> ' +
+                '<input type="password" class="login__input"> ' +
+            '</div> ' +
+            '<div id="1" class="login-block__login-button"><p>Sign in!</p></div> ' +
+        '</form> ' +
     '</div> ';
