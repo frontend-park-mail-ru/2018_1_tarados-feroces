8b21c42ba47f4e5b03a8eafb05f61f66bf604d7f
kabachok169 <kabachok890098@yandex.ru>
2018-02-21 20:09:01 +0300

    added header component

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,9 @@
+class Header extends BaseComponent {
+    constructor() {
+        super(headerTemplate);
+    }
+}
+
+const headerTemplate = '<div class="header {{class}}">' +
+    '<h1>{{text}}</h1>' +
+    '</div>';
