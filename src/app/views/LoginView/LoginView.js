61e12a01e74439b762bc36d060f1dcdac1b623ed
Danchetto <danchetto@yandex.ru>
2018-02-24 17:31:37 +0300

    +input event listner

--- src/app/views/LoginView/LoginView.js
+++ src/app/views/LoginView/LoginView.js
@@ -8,10 +8,10 @@ const loginTemplate = '<div class="page">' +
     '<Header>Login</Header>' +
     '<div class="registration-block">' +
     '<Form>' +
-    '<Input label-text="Full name:" type="text" placeholder="Enter name"></Input>' +
-    '<Input label-text="Password:" type="password" placeholder="Enter password"></Input>' +
+    '<Input label-text="Full name:" type="text" placeholder="Enter name" blur="() {console.log(1000)};"></Input>' +
+    '<Input label-text="Password:" type="password" placeholder="Enter password" blur="() {console.log(1000)};"></Input>' +
     '<div class="button-container">' +
-    '<Button class="button large" click="function() {alert(1);}">Log In!</Button>' +
+    '<Button class="button large" click="() {alert(1);}">Log In!</Button>' +
     '<Button class="button large" click="(event){ event.preventDefault(); goBack();  }">Back</Button>' +
     '</div>' +
     '</Form>' +
