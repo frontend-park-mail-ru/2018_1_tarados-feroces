06fb91e1a6a73f8e4a244e35cedcb0b46f37b0b8
kabachok169 <kabachok890098@yandex.ru>
2018-02-24 15:20:04 +0300

    fix len of str in view

--- src/app/views/LoginView/LoginView.js
+++ src/app/views/LoginView/LoginView.js
@@ -8,8 +8,10 @@ const loginTemplate = '<div class="page">' +
     '<Header>Login</Header>' +
     '<div class="registration-block">' +
     '<Form>' +
-    '<Input input-class="user-name-input" label-text="Full name:" type="text" placeholder="Enter name"></Input>' +
-    '<Input input-class="user-password-input" label-text="Password:" type="password" placeholder="Enter password"></Input>' +
+    '<Input input-class="user-name-input" label-text="Full name:" type="text" placeholder="Enter name">' +
+    '</Input>' +
+    '<Input input-class="user-password-input" label-text="Password:" type="password" placeholder="Enter password">' +
+    '</Input>' +
     '<div class="button-container">' +
     '<Button class="button large" click="function() {validate();}">Log In!</Button>' +
     '<Button class="button large" click="(event){ event.preventDefault(); goBack();  }">Back</Button>' +
