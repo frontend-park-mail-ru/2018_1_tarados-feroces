f71574f6d40902f067b6da8a4dc54183617fa784
kabachok169 <kabachok890098@yandex.ru>
2018-02-24 15:07:45 +0300

    added simple validation

--- src/app/views/LoginView/LoginView.js
+++ src/app/views/LoginView/LoginView.js
@@ -8,12 +8,34 @@ const loginTemplate = '<div class="page">' +
     '<Header>Login</Header>' +
     '<div class="registration-block">' +
     '<Form>' +
-    '<Input label-text="Full name:" type="text" placeholder="Enter name"></Input>' +
-    '<Input label-text="Password:" type="password" placeholder="Enter password"></Input>' +
+    '<Input input-class="user-name-input" label-text="Full name:" type="text" placeholder="Enter name"></Input>' +
+    '<Input input-class="user-password-input" label-text="Password:" type="password" placeholder="Enter password"></Input>' +
     '<div class="button-container">' +
-    '<Button class="button large" click="function() {alert(1);}">Log In!</Button>' +
+    '<Button class="button large" click="function() {validate();}">Log In!</Button>' +
     '<Button class="button large" click="(event){ event.preventDefault(); goBack();  }">Back</Button>' +
     '</div>' +
     '</Form>' +
     '</div>' +
     '</div>';
+
+const validate = () => {
+    const username = document.querySelector('.user-name-input');
+    const password = document.querySelector('.user-password-input');
+    let validationControl = 0;
+
+    if (username.value === '') {
+       alert('empty username');
+    } else {
+        validationControl += 1;
+    }
+
+    if (password.value === '') {
+        alert('empty password');
+    } else {
+        validationControl += 1;
+    }
+
+    if (validationControl === 2) {
+        alert('1');
+    }
+};
