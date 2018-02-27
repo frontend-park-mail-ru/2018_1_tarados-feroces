b9dbe6c9f655e9853e7424c597a0acb41e21ae60
kabachok169 <kabachok890098@yandex.ru>
2018-02-27 18:11:56 +0300

    refactored style

--- src/app/components/BaseComponent/BaseComponent.js
+++ src/app/components/BaseComponent/BaseComponent.js
@@ -11,10 +11,8 @@ class BaseComponent {
 
     render(context) {
         const div = document.createElement('div');
-        // debugger;
         div.innerHTML = templateManager.getHTML(context, this.template);
         this._element = div.lastChild;
-        console.log(div.innerHTML);
         this.addListeners(context);
         return this._element;
     }
