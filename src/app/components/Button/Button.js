6947053a31767007fe58fd34080b50d30f89013d
kabachok169 <kabachok890098@yandex.ru>
2018-04-03 15:10:06 +0300

    wrong b

--- src/app/components/Button/Button.js
+++ src/app/components/Button/Button.js
@@ -4,7 +4,7 @@ import BaseComponent from '../BaseComponent/BaseComponent'
 export default class Button extends BaseComponent {
 
     render(context) {
-        this.template = `<div class="button {{class}}"><p>{{text}}</p></div>`;
+        this.template = `<div class="button {{class}}"><p class="button__value">{{text}}</p></div>`;
         super.render(context);
     }
 }
