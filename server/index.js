98ddecdde7d4ea426391e149291d492348a329c3
kabachok169 <kabachok890098@yandex.ru>
2018-04-06 11:23:23 +0300

    fixed color user

--- server/index.js
+++ server/index.js
@@ -4,20 +4,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/', express.static(path.join('dist')));
-app.use('/login', express.static(path.join('dist')));
-app.use('/signup', express.static(path.join('dist')));
-app.use('/test', express.static(path.join('dist')));
-app.use('/leaderboard', express.static(path.join('dist')));
-app.use('/settings', express.static(path.join('dist')));
-app.use('/user', express.static(path.join('dist')));
-app.use(/\/.+/gi, express.static(path.join('dist')));
+app.use(express.static(path.join('dist')));
+// app.use('/login', express.static(path.join('dist')));
+// app.use('/signup', express.static(path.join('dist')));
+// app.use('/test', express.static(path.join('dist')));
+// app.use('/leaderboard', express.static(path.join('dist')));
+// app.use('/settings', express.static(path.join('dist')));
+// app.use('/user', express.static(path.join('dist')));
+// app.use(/\/.+/gi, express.static(path.join('dist')));
 
-app.get('/login/id', function(req, res) {
-    res.status(200).json({text: 'hello world'});
-});
+
 
 app.listen(process.env.PORT || 8080, () => console.log('Example app listening on port 8080!'));
-
-//    "watch:lint": "node node_modules/eslint-watch/bin/esw -w --fix"
-//    "eslint-watch": "^3.1.3",
