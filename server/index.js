fef88508f77922e0fe39177772c4269037c3168e
kabachok169 <kabachok890098@yandex.ru>
2018-04-06 11:42:28 +0300

    + 404

--- server/index.js
+++ server/index.js
@@ -4,15 +4,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join('dist')));
-// app.use('/login', express.static(path.join('dist')));
-// app.use('/signup', express.static(path.join('dist')));
-// app.use('/test', express.static(path.join('dist')));
-// app.use('/leaderboard', express.static(path.join('dist')));
-// app.use('/settings', express.static(path.join('dist')));
-// app.use('/user', express.static(path.join('dist')));
+app.use('/', express.static(path.join('dist')));
+app.use('/login', express.static(path.join('dist')));
+app.use('/signup', express.static(path.join('dist')));
+app.use('/test', express.static(path.join('dist')));
+app.use('/leaderboard', express.static(path.join('dist')));
+app.use('/settings', express.static(path.join('dist')));
+app.use('/user', express.static(path.join('dist')));
 // app.use(/\/.+/gi, express.static(path.join('dist')));
+// app.use(express.static(path.join('dist')));
 
-
+app.use(function(req, res) {
+    res.status(404).send('404');
+});
 
 app.listen(process.env.PORT || 8080, () => console.log('Example app listening on port 8080!'));
