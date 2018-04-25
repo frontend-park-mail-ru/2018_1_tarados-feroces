13616bc8bf6e22b70deff9d46474c9c35e20bf76
kabachok169 <kabachok890098@yandex.ru>
2018-04-25 23:22:41 +0300

    working

--- server/index.js
+++ server/index.js
@@ -18,4 +18,34 @@ app.use('*', function(req, res) {
     res.status(404).send('404');
 });
 
+const WebSocketServer = require('ws');
+
+// подключенные клиенты
+const clients = {};
+
+// WebSocket-сервер на порту 8081
+const webSocketServer = new WebSocketServer.Server({
+    port: 8081
+});
+webSocketServer.on('connection', function(ws) {
+
+    const id = Math.random();
+    clients[id] = ws;
+    console.log(`new conn ${id}`);
+
+    ws.on('message', function(message) {
+        console.log(`message received ${message}`);
+
+        for (const key in clients) {
+            clients[key].send(message);
+        }
+    });
+
+    ws.on('close', function() {
+        console.log(`conn closed ${id}`);
+        delete clients[id];
+    });
+
+});
+
 app.listen(process.env.PORT || 8080, () => console.log('Example app listening on port 8080!'));
