6b553ecfc55b0dca6b66a664c92a50084126797f
Danchetto <danchetto@yandex.ru>
2018-05-17 14:13:52 +0300

    + state header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,17 +1,27 @@
 import * as React from 'react';
 
 import './Header.scss';
+import UserInfo from '../UserInfo/UserInfo';
+import Logo from '../Logo/Logo';
 
 interface Props {
     className?: string;
+    isAuth?: boolean;
+    avatar?: string;
+    coins?: string;
+    points?: string;
+    login?: string;
 }
 
 export default class Header extends React.Component<Props, any> {
 
     render() {
-        const {className}: any = this.props;
+        const {className, isAuth, avatar, points, coins, login}: any = this.props;
         return (
-            <div className={'header ' + className}/>
+            <div className={'header ' + className}>
+                <Logo/>
+                {isAuth ? <UserInfo avatar={avatar} points={points} coins={coins} login={login} /> : ''}
+            </div>
         );
     }
 };
