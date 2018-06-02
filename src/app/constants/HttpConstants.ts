c5781bafe745a20cca491db1444ead4c45f11866
Danchetto <danchetto@yandex.ru>
2018-06-02 03:47:16 +0300

    fixees

--- src/app/constants/HttpConstants.ts
+++ src/app/constants/HttpConstants.ts
@@ -1,7 +1,7 @@
-// const HTTP_DOMEN: string = exports.HTTP_DOMEN = '/api';
-// const WS_ADDRESS: string = exports.WS_ADDRESS = `ws${location.protocol === 'https:' ? 's' : ''}://deadlinez.net/ws`;
-export const HTTP_DOMEN: string = 'http://api.deadlinez.net:8080/api';
-export const WS_ADDRESS: string = 'ws://api.deadlinez.net:8080/session';
+const HTTP_DOMEN: string = exports.HTTP_DOMEN = '/api';
+const WS_ADDRESS: string = exports.WS_ADDRESS = `ws${location.protocol === 'https:' ? 's' : ''}://deadlinez.net/ws`;
+// export const HTTP_DOMEN: string = 'http://api.deadlinez.net:8080/api';
+// export const WS_ADDRESS: string = 'ws://api.deadlinez.net:8080/session';
 export const POST: string = 'POST';
 export const GET: string = 'GET';
 export const HEADER_CONTENT_TYPE: string = 'Content-Type';
